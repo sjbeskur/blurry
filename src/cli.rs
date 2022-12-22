f0a3938184edd3752cd5da906140f53b97738c57
sjbeskur <sam.beskur@gmail.com>
2022-12-21 23:20:38 -0700

    bugs

--- src/cli.rs
+++ src/cli.rs
@@ -7,21 +7,23 @@ pub struct Config{
 
     
     #[command(subcommand)]
-    pub command: Option<Commands>,
+    pub command: Commands,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum Commands{
-    Avg {
-        #[arg(short, long)]
+    #[command(author, version, about, long_about = "Blurs using normalized box filter")]
+    Norm {
+        #[arg(short, long, help="box filter kernel size (should be odd number)")]
         ksize: i32,
     },
 
+    #[command(author, version, about, long_about = "Blurs an image using a Gaussian filter")]
     Gaus{
         #[arg(short='k', long)]
         ksize: i32,
    
-        #[arg(short='s', long)]
+        #[arg(short='s', long, default_value_t=0.0)]
         sigma: f64,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,19 @@ fn main() {
     dbg!(&cfg);
 
     match cfg.command {
-        Some(blurry::cli::Commands::Avg{ ksize }) => {
+        
+        blurry::cli::Commands::Norm{ ksize } => {
             println!("{}", ksize);
             blurry::blur_avg(cfg, ksize);
         },
-        Some(blurry::cli::Commands::Gaus{ksize, sigma}) => {
+
+        blurry::cli::Commands::Gaus{ksize, sigma} => {
             println!("Gaussian");
             blurry::blur_gaussian(cfg, ksize, sigma, sigma);
         }
-        None=>{}
         
     }
+
 /* 
     if let Err(e) = blurry::run(cfg){
         eprintln!("{}", e);
